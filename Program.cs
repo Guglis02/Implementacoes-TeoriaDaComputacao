9fb77e47e55023d610ea52dcbac0811e1787a0e9
Gustavo Machado <gmfreitas@inf.ufsm.br>
2022-10-24 16:00:51 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDeTuringReversivel
+{
+    internal class Program
+    {
+        private static string path = @"./a.txt";
+
+        static void Main(string[] args)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
+    }
+}
