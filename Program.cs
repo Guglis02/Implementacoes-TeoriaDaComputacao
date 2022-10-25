d6323d20c76250a67a81e9894981339ec8da0b70
Gustavo Machado <gustavomf02@gmail.com>
2022-10-25 16:30:54 -0300

    Implementa extração de algumas informações do input

--- Program.cs
+++ Program.cs
@@ -7,22 +7,65 @@ using System.Threading.Tasks;
 
 namespace MaquinaDeTuringReversivel
 {
+    /// <summary>
+    /// Pra rodar o programa é só ir na pasta que ele ta e usar
+    /// .\MaquinaDeTuringReversivel.exe .\entrada-quintupla.txt
+    /// </summary>
     internal class Program
     {
-        private static string path = @"./a.txt";
+        //private static string path = @"./entrada-quintupla.txt";
+        static int numberOfStates;
+        static int inputAlphabetSize;
+        static int tapeAlphabetSize;
+        static int numberOfTransitions;
+
+        static string[] input;
+        static string[] states;
+        static string[] inputAlphabet;
+        static string[] tapeAlphabet;
+
+        static void GetQuantities(string line)
+        {
+            string[] quantities = line.Split(' ');
+
+            numberOfStates = Int32.Parse(quantities[0]);
+            inputAlphabetSize = Int32.Parse(quantities[1]);
+            tapeAlphabetSize = Int32.Parse(quantities[2]);
+            numberOfTransitions = Int32.Parse(quantities[3]);
+        }
 
         static void Main(string[] args)
         {
-            using (FileStream fs = File.Open(path, FileMode.Open))
-            {
-                byte[] b = new byte[1024];
-                UTF8Encoding temp = new UTF8Encoding(true);
+            List<string> lines = File.ReadAllLines(args[0]).ToList();
 
-                while (fs.Read(b, 0, b.Length) > 0)
-                {
-                    Console.WriteLine(temp.GetString(b));
-                }
-            }
+            GetQuantities(lines[0]);
+            lines.RemoveAt(0);
+
+            states = lines[0].Split();
+            lines.RemoveAt(0);
+
+            inputAlphabet = lines[0].Split(' ');
+            lines.RemoveAt(0);
+
+            tapeAlphabet = lines[0].Split(' ');
+            lines.RemoveAt(0);
+
+            input = lines.Last().Split();
+            lines.RemoveAt(lines.Count() - 1);
+
+            Console.WriteLine($"Numero de estados = {numberOfStates}\n" +
+                $"Tamanho alfabeto de entrada = {inputAlphabetSize}\n" +
+                $"Tamanho alfabeto da fita = {tapeAlphabetSize}\n" +
+                $"N de transicoes = {numberOfTransitions}\n" +
+                $"Estados = {states[0]}\n" +
+                $"Alfabeto de entrada = {inputAlphabet}\n" +
+                $"Alfabeto da fita = {tapeAlphabet}\n" +
+                $"Entrada = {input}\n");
+
+            /*foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }*/
         }
     }
 }
